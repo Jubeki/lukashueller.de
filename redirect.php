69892915bdedf4b6d632aa56f468669821b7d824
Julius <jubeki99@gmail.com>
2019-11-06 14:32:00 +0100

    Add password script

---
+++ redirect.php
@@ -0,0 +1,40 @@
+<?php
+
+if(isset($_POST['password'])) {
+    $password = $_POST['password'];
+    switch($_GET['redirect']) {
+        case 'google':
+            $password_check = "pw123";
+            $redirect = "https://www.google.com/";
+            break;
+        case 'lukashueller':
+            $password_check = "pw456";
+            $redirect = "https://lukashueller.de/";
+            break;
+    }
+
+    if(isset($password_check) && $password_check == $password) {
+        header('Location: ' . $redirect);
+        die();
+    }
+
+    http_response_code(403);
+    die('Wrong password');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Weiterleitung mit Passwort</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="password">Enter password: </label>
+        <input type="password" name="password">
+    </form>
+</body>
+</html>
